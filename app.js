d39c78bee8ddb5d62c010ddafb06327ab72ddd6c
cnjara <cnjara@outlook.com>
2022-07-22 00:54:51 -0300

    Versión inicial del diseño base + estructura sitio

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ app.use( express.static( 'public' ));
 app.get( '/', ( req, res ) => {
 	res.sendFile( ruta.join( __dirname, 'views', 'index.html' ))
 });
+app.get( '/nosotros', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'nosotros.html' ))
+});
+app.get( '/productos', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'productos.html' ))
+});
+app.get( '/catering', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'catering.html' ))
+});
+app.get( '/contacto', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'contacto.html' ))
+});
+app.get( '/como-comprar', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'como-comprar.html' ))
+});
+app.get( '/login', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'login.html' ))
+});
+app.get( '/carrito', ( req, res ) => {
+	res.sendFile( ruta.join( __dirname, 'views', 'carrito.html' ))
+});
