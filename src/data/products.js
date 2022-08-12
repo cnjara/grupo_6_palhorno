a1b3d884d09f9b1bfd53596d4dafa19362b9b33b
cnjara <cnjara@outlook.com>
2022-08-11 23:53:10 -0300

    Estructuración mvc del proyecto

---
+++ src/data/products.js
@@ -0,0 +1,23 @@
+module.exports = [
+	{
+		id : 1,
+		title : "1",
+		description : "1",
+		price : 1,
+		image: "1.jpg"
+	},
+	{
+		id : 2,
+		title : "2",
+		description : "2",
+		price : 2,
+		image: "2.jpg"
+	},
+	{
+		id : 3,
+		title : "3",
+		description : "3",
+		price : 3,
+		image: "3.jpg"
+	}
+]
