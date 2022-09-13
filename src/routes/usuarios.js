c8113eb377e6b1d900a468f5070d5b5ed4213c32
Bettina98 <beona67@gmail.com>
2022-09-13 20:58:47 -0300

    corrección formulario login

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {login, registro,procesoRistro,profile} = require('../controllers/usuariosController');
+const {login, registro,procesoRistro,profile,procesoLogin} = require('../controllers/usuariosController');
 const registerValidator =require('../validations/registerValidator');
 //const imageUsuario = require('/..multe/imageUsuarios')
 
