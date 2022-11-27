6b7b8e5f5046a33704e4425b8cb54593e28993f7
leonardomg888 <leonardomg888@gmail.com>
2022-11-27 22:31:00 +0100

    error

--- src/controllers/api/apiUsuariosController.js
+++ src/controllers/api/apiUsuariosController.js
@@ -2,57 +2,22 @@ const db = require('../../database/models');
 const { literal,} = require('sequelize');
 const fs = require ('fs');
 const path = require ('path');
-const createError = require('../../helpers/createError')
+//const createError = require('../../helpers/createError')
 
 
 module.exports= {
 
     list :async(req,res) => {
    
+   /*
     try {
         const  usuarios =  await db.User.findAll({
-    
-           include:[{
-                association: "rolId",
-                attributes:{
-                    exclude:["","createdAt","updatedAt","deletedAt"]            
-                            
-                }
-    
-            },
-            {
-                association: "avatar",
-                attributes:{
-                    exclude:["id","createdAt","updatedAt","productId"],
-                    include:[[literal(`CONCAT('${req.protocol}://${req.get('host')}/api/productos/imagen/',archivo)`),'url']]
-    
-                }
-    
-                
-            }
-        ],
-    
-        attributes:{
-            exclude:["categoryId","createdAt", "updatedAt", "id"
-    
-            ]
-        }
-    
-    
         })
-    
-           
         return res.json({usuarios})
-    
-    }catch (error) {
-        console.log(error)
-        return res.status(error.status || 500).json({
-            status:error.status || 500,
-            msg:error.message
-        })
-    }
-},     
-        getOne : async(req,res) => {
+     } catch (error) {
+   console.log (error)
+}
+      /*  getOne : async(req,res) => {
      const {id} = req.params;
      //return res.json("estamos llegando")
             
